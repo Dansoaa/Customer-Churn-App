25195082d4282f449decd52d668c0171d52a92f0
Dansoaa <dansoaa2012@gmail.com>
2024-03-01 16:55:11 +0000

    server details

--- pages/01_data.py
+++ pages/01_data.py
@@ -5,48 +5,28 @@ import pandas as pd
 import pyodbc
 
 # Title of the page
-st.title("Data Page")
+st.title("Vodafone Churn Data")
 
-## Define a function to establish a database connection and cache it
-@st.cache_data()
-def connect_to_database():
-    # Load database credentials from secrets.toml file
-    environment_variables = st.secrets['connection']
+def initialize_connection():
+     connection = pyodbc.connect(
+          "DRIVER={{SQL Server}};SERVER="
+          +st.secrets['SERVER']
+          +";DATABASE="
+          +st.secrets['DATABASE']
+          +";UID="
+          +st.secrets['LP_project']
+          +";PWD="
+          +st.secrets['PWD']
+     )
+     return connection
 
-    # Extract credentials
-    server = environment_variables["SERVER"]
-    database = environment_variables["DATABASE"]
-    username = environment_variables["UID"]
-    password = environment_variables["PWD"]
+conn = initialize_connection()
 
-    # Connection string
-    connection_string = f"DRIVER={{SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}"
+def query_database(query):
+     with conn.cursor() as cur:
+        cur.execute(query)
+        rows = cur.fetchall()
 
-    # Establish a connection
-    connection = pyodbc.connect(connection_string)
-    
-    query = "SELECT * FROM LP2_Telco_churn_first_3000"
+        df=pd.DataFrame.from_records(data=rows, columns=[column[0] for column in cur.description])
 
-    data = pd.read_sql(query, connection)
-    return data
-
-
-# Create selection option
-column1, column2 = st.columns(2)
-with column2:
-        option = st.selectbox('Choose columns to be viewed',
-                              ('All Columns','Numeric Columns','Categorical Columns'))
- 
-df = connect_to_database()
- 
-# Display based on selection
-if option == 'Numeric Columns':
-    st.subheader('Numeric Columns')
-    st.write(df.select_dtypes(include='number'))
-elif option == 'Categorical Columns':
-    st.subheader('Categorical Columns')
-    st.write(df.select_dtypes(include='object'))
-else:
-     st.subheader('Entire Dataset')
-     st.write(df)
-
+     return df
